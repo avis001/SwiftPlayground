204dafefc05f86d351d1bd8869f110d658455afd
SivasankarZ <sivasankar.kumaravel@zalora.com>
2024-10-02 21:09:03 +0530

    Commit Zero

---
+++ helloWorld.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+print("Hello World")
+
+func add(lhs: Double, rhs: Double) -> Double {
+    lhs + rhs
+}
+
+let x = "Some String"
+
+print( x.prefix(5) )
+
+print( add(lhs: 5.0, rhs: 1.0) )
+
+func subtract(lhs: Double, rhs: Double) -> Double {
+    lhs - rhs
+}
+
+print(subtract(lhs: 234, rhs: 215))
